c99e035432ae178d03401e245d21c213f4bbfbe7
Teemu Frisk <teemu.frisk@gmail.com>
2013-07-10 06:59:57 +0000

    Try to pipe index.html to browser

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var outputfile = "index.html";
+buf = new Buffer(fs.readFileSync(outputfile), "utf-8");
+var outputdata = buf.toString('utf-8');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(outputdata);
 });
 
 var port = process.env.PORT || 5000;
