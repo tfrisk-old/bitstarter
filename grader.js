6d87c59043b538e46d6bad521f3e7eb8b26698aa
Teemu Frisk <teemu.frisk@gmail.com>
2013-07-10 12:00:21 +0000

    Try to get async hell working

--- grader.js
+++ grader.js
@@ -46,13 +46,25 @@ var loadChecks = function(checksfile) {
 };
 
 /* Taken from restler example */
-var loadUrl = function(urlstr) {
+var checkUrl = function(urlstr, checksfile) {
     rest.get(urlstr).on('complete', function(result) {
 	if (result instanceof Error) {
 	    console.log('Error: ' + result.message);
 	    this.retry(5000); // try again after 5 sec
 	} else {
-	    console.log(result);
+	    //process checks
+	    //console.log('checkUrl results: ' + result);
+	    $ = cheerio.load(result);
+	    console.log('checkUrl $: ' + $);
+	    var checks = loadChecks(checksfile).sort();
+	    console.log('checkUrl checks: ' + checks);
+	    var out = {};
+	    for (var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	    }
+	    console.log('checkUrl out' + out);
+	    return out;
 	}
     });
 };
@@ -78,11 +90,21 @@ if(require.main == module) {
     program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-    .option('-u, --url <url>', 'External url to be checked', clone(loadUrl))
+    .option('-u, --url <url>', 'External url to be checked')
     .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if(program.url) {
+        console.log('program.url: ' + program.url);
+        //var tmpdata = loadUrl(program.url);
+        //console.log('tmpdata: ' + tmpdata);
+        //var tmpfile = fs.writeFile('tmpfile',tmpdata);
+        //var checkJson = checkHtmlFile('tmpfile', program.checks);
+        var checkJson = checkUrl(program.url, program.checks);
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    console.log('outJson: ' + outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkUrl = checkUr;
 }
